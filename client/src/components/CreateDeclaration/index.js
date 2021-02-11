2278ca920917643886c3e2306055297f3a770057
Ninetta11 <ninawelsh1@outlook.com>
2021-02-11 15:25:35 +1100

    Added API key

--- client/src/components/CreateDeclaration/index.js
+++ client/src/components/CreateDeclaration/index.js
@@ -153,7 +153,6 @@ function CreateDeclaration() {
                                 :
                                 <div></div>
                         }
-
                         < Form.Item
                             label="Title:"
                             name="title"
