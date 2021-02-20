c8428feba7576d7ddc2c7ce1b9944a618839db7d
Ninetta11 <ninawelsh1@outlook.com>
2021-02-20 13:49:31 +1100

    Updated sider with all user related menu items moved under user and Update Details link not opens drawer rather than going to another page

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Link, Switch, useHistory } from 'react-
 import { Row, Col, Layout, Menu } from 'antd';
 import { UserOutlined, FileOutlined, IdcardOutlined, FileAddOutlined, MailOutlined, LogoutOutlined } from '@ant-design/icons';
 import CreateDeclaration from '../components/CreateDeclaration';
-import UpdateDetails from '../components/UpdateDetails';
+import UserDetails from '../components/UserDetails';
 import DisplayDeclaration from '../components/DisplayDeclaration';
 import Request from '../components/Request';
 import { useAppContext } from '../store';
@@ -33,6 +33,18 @@ function Dashboard() {
         history.push('/');
     };
 
+    const [drawerState, setDrawerState] = useState({
+        visible: false
+    });
+
+    const showDrawer = () => {
+        setDrawerState({ visible: true });
+    };
+
+    const onClose = () => {
+        setDrawerState({ visible: false });
+    };
+
     const handleDocumentSelect = (e) => {
         const hash = e.key;
         const title = e.item.props.name;
@@ -44,6 +56,9 @@ function Dashboard() {
 
     return (
         <Layout >
+            <UserDetails
+                drawerState={drawerState}
+                onClose={onClose} />
             <Router>
                 <Sider className="site-layout-background" width={250} breakpoint="lg"
                     collapsedWidth="0"
@@ -56,36 +71,34 @@ function Dashboard() {
                         style={{ height: '100%' }}
                     >
                         <SubMenu key="sub1" icon={<UserOutlined />} title={state.user.first_name + ' ' + state.user.last_name}>
-                            <Menu.Item key="2" icon={<IdcardOutlined />}><Link to="/user/update">Update Details</Link></Menu.Item>
-                        </SubMenu>
-                        <SubMenu key="sub2" icon={<FileOutlined />} title="Documents">
-                            {(state.user.documents ?
-                                state.user.documents.map(document =>
-                                    <Menu.Item
-                                        name={document.title}
-                                        key={document.hash}
-                                        onClick={handleDocumentSelect}
-                                    ><Link to="/user/document">{document.title}</Link></Menu.Item>)
-                                :
-                                <Menu.Item key="noDocuments">You have no documents</Menu.Item>
-                            )}
+                            <Menu.Item key="2" icon={<IdcardOutlined />} onClick={showDrawer}>Update Details</Menu.Item>
+                            <SubMenu key="sub2" icon={<FileOutlined />} title="Documents">
+                                {(state.user.documents ?
+                                    state.user.documents.map(document =>
+                                        <Menu.Item
+                                            name={document.title}
+                                            key={document.hash}
+                                            onClick={handleDocumentSelect}
+                                        ><Link to="/user/document">{document.title}</Link></Menu.Item>)
+                                    :
+                                    <Menu.Item key="noDocuments">You have no documents</Menu.Item>
+                                )}
+                            </SubMenu>
+                            < Menu.Item
+                                key="5" icon={<LogoutOutlined />}
+                                id="logoutBtn"
+                                onClick={handleLogOut}>Logout
+                            </Menu.Item>
                         </SubMenu>
                         < Menu.Item key="3" icon={<FileAddOutlined />}><Link to="/user/declaration">Create New Document</Link></Menu.Item>
                         < Menu.Item key="4" icon={<MailOutlined />}><Link to="/user/request">Request Document</Link></Menu.Item>
-                        < Menu.Item
-                            key="5" icon={<LogoutOutlined />}
-                            id="logoutBtn"
-                            onClick={handleLogOut}>Logout
-                </Menu.Item>
+
                     </Menu>
                 </Sider >
                 <Content className="content" >
                     <Row>
                         <Col span={20} offset={2}>
                             <Switch>
-                                <Route exact path="/user/update">
-                                    <UpdateDetails />
-                                </Route>
                                 <Route exact path="/user/document">
                                     <DisplayDeclaration
                                         hash={documentState.hash}
